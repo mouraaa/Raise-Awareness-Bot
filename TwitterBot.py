473c993b151189912b05131485b9a13ab6f7f4b9
Amr Abedo <codemouraa@gmail.com>
2020-12-27 17:38:11 -0500

    Added while loop and placed get mentions in try statement

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,4 +1,4 @@
-import tweepy, random
+import tweepy, random, time
 
 ACCESS_TOKEN = 'enter_acces_token_here'
 ACCESS_TOKEN_SECRET = 'enter_acces_token_secret_key_here'
@@ -18,30 +18,28 @@ file = open("articles.text")
 for line in file:
 	articles.append(line.rstrip())
 
-#read the file containing the latest tweet id
-with open("id_file.text") as f:
-	last_seen_id = f.readline().rstrip()
+while True: 
+	#read the file containing the latest tweet id
+	with open("id_file.text") as f:
+		last_seen_id = f.readline().rstrip()
 
-#returns the 20 most recent mentions, including retweets. (basically anytime someone @'s you)
-mentions = api.mentions_timeline(last_seen_id)
+	try: #check if there are any new tweets and reply if conditions are met
+		#returns the 20 most recent mentions, including retweets. (basically anytime someone @'s you)
+		mentions = api.mentions_timeline(last_seen_id)
+		
+		last_seen_id = mentions[0].id 
+		if last_seen_id:
+			counter = 0
+			for mention in mentions:
+				if substring in mention.text.lower():
+					article = random.choice(articles)
+					api.update_status("Here's todays article. \n" + article, mention.id, True)
+					counter+=1
+			print("Replied to " + str(counter) + " tweets")		
+	except:
+		print("No new tweets contain the specified hashtag")
 
-#check if there are any new tweets and reply if conditions are met
-try: 
-	last_seen_id = mentions[0].id 
-	if last_seen_id:
-		counter = 0
-		for mention in mentions:
-			if substring in mention.text.lower():
-				article = random.choice(articles)
-				api.update_status("Here's todays article. \n" + article, mention.id, True)
-				counter+=1
-		print("Replied to " + str(counter) + " tweets")		
-except:
-	print("No new tweets contain the specified hashtag")
+	with open("id_file.text", "w") as f:
+		f.write(str(last_seen_id))
 
-with open("id_file.text", "w") as f:
-	f.write(str(last_seen_id))
-
-
-
-	
+	time.sleep(15)
